a51752eb8b74c77547faf4fdfd4c7b82b359b48c
jon <jonschoelwer1@gmail.com>
2022-06-29 15:03:14 -0500

    added index.js and exports handler

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+exports.handler = async (event) => {
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    };
+    if (event.httpMethod === "OPTIONS") {
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({ message: "Successful preflight call." }),
+      };
+    } else if (event.httpMethod === "POST") {
+      const { name } = JSON.parse(event.body);
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({ message: "Hello, " + name }),
+      };
+    }
+  };
+  
