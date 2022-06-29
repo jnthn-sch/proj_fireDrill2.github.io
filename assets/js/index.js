28140e49bcc4d139b932429327c16b3d785c3c77
jon <jonschoelwer1@gmail.com>
2022-06-29 15:09:52 -0500

    exports handler input

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,19 +4,6 @@ exports.handler = async (event) => {
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     };
-    if (event.httpMethod === "OPTIONS") {
-      return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({ message: "Successful preflight call." }),
-      };
-    } else if (event.httpMethod === "POST") {
-      const { name } = JSON.parse(event.body);
-      return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({ message: "Hello, " + name }),
-      };
-    }
+    
   };
   
