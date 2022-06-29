8d4dcb3f5f400a9fd25f393ffaf53ed88d890802
Lumley <mason.lumley@kochgs.com>
2022-06-29 13:23:22 -0500

    same as berfore

--- assets/js/main.js
+++ assets/js/main.js
@@ -485,20 +485,32 @@ function updateJSON(){
 
 	var xhr = new XMLHttpRequest();
     var jsonRequestURL = "jsons/status.json";
+	xhr.open("GET", jsonRequestURL);
+	xhr.addEventListener('load', ()=>{
+		if(xhr.readyState == 4 && xhr.status == 200)
+   		 {
+			jsonArr = JSON.parse(xhr.responseText);
+			xhr.send();
+    		jsonArr.push({"name":"Cody", "status":"Safe"});
+
+   			xhr.open("POST", jsonRequestURL, true);
+			xhr.setRequestHeader("Accept", "application/json");
+			xhr.setRequestHeader("Content-Type", "application/json");
+    		xhr.send("jsonTxt="+JSON.stringify(jsonArr));
+			useXHR();
+		}
+	});
+	
+
 	
 	
-		
-	jsonArr = JSON.parse(xhr.responseText);
 
-    jsonArr.push({"name":"Cody", "status":"Safe"});
 
-   	xhr.open("POST", jsonRequestURL, true);
-	xhr.setRequestHeader("Accept", "application/json");
-	xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send("jsonTxt="+JSON.stringify(jsonArr));
-	useXHR();
-		
+	
+	
+	
 
+	
 }
 
 useXHR();
