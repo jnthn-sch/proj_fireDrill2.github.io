3acf78f9c246a42b7c0f6fa416fcbd374a229d17
jon <jonschoelwer1@gmail.com>
2022-06-29 15:14:40 -0500

    what

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    };
-    
-  };
-  
