2f4529cde055720a7d330270471cf1f1c28acd6e
Lumley <mason.lumley@kochgs.com>
2022-06-30 14:25:51 -0500

    changing origin in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -496,7 +496,7 @@ function updateJSON(){
 			
    			xhr.open("POST", jsonRequestURL, true);
 			xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); 
-			xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://jnthn-sch.github.io/proj_fireDrill2.github.io/jsons/status.json');
+			xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://www.firedrillgroup24.com');
     		xhr.setRequestHeader('Access-Control-Allow-Methods' , 'POST, OPTIONS')
 			xhr.send("jsonTxt="+JSON.stringify(jsonArr));
 			useXHR();
